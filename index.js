0f6b1ac25a84aae669838a44ab98764e1508ccbd
Natasha Oxley <natashareneeoxley@gmail.com>
2017-11-18 17:39:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const currentUser ='Natasha Oxley';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+debugger
 const shortGreeting = `Welcome, ${currentUser.slice(0,1}`;
